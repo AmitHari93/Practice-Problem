53fb87e85ddf61201ce2a7a27e762b8364db45e2
Amit Hari <amithari25@gmail.com>
2024-06-29 00:41:32 +0530

    m

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,8 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Practice {
 
     public static void patternDiamond(Integer n){
@@ -95,12 +100,92 @@ public class Practice {
 
     }
 
+    public static void pattern_5(int n){
+        for(int i=n; i > 0; i--){
+            for(char c = 'A'; c <'A'+i; c++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern_6(int n){
+        char c = 'A';
+        for(int i=1; i<= n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(c);
+            }
+            c++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern_7(int n){
+        for(int i=0; i<n; i++){
+            for(int s=n-i; s > 0; s--){
+                System.out.print(" ");
+            }
+            char c = 'A';
+            for(int j=0; j<=i; j++){
+                System.out.print(c);
+                c++;
+            }
+            c--;
+            for(char k=0; k<i; k++){
+                c--;
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern_8(int n){
+//        char c = 'A';
+//        for(int i=0; i<n; i++){
+//            c++;
+//        }
+//        c--;
+//        for(int j =0; j<n; j++){
+//            char p = c;
+//            for(char k = 0; k<=j ; k++){
+//                System.out.print(p);
+//                p++;
+//            }
+//            c--;
+//            System.out.println();
+//        }
+        for(int i=0; i<n; i++){
+            for(char c = (char)('A'+ n-1-i); c <= (char)(int)'A'+n-1; c++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void prefix_sum(int[] array){
+        int sum = 0;
+        int ans = -1;
+        int prefix_sum = 0;
+        for (int k : array) {
+            sum += k;
+        }
+        for (int j=0; j< array.length; j++) {
+            sum  = sum - array[j];
+            if(sum == prefix_sum) ans = j;
+            prefix_sum += array[j];
+        }
+        if(ans != -1) System.out.println(ans);
+        else System.out.println("No");
+    }
+
     public static void main(String[] args){
 //        patternDiamond(3);
 //        halfDiamond(5);
-//        pattern_1(7);
-//        pattern_2(5);
-//        pattern_3(4);
-        pattern_4(4);
+//        pattern_8(4);
+        int[] array = {10,7,2,1,5,4,10};
+//        for (int j : array) {
+//            System.out.println(j);
+//        }
+        prefix_sum(array);
     }
 }
